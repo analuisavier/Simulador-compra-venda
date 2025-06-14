706eabfbb74413772d767795dcdd41aeac1f9f94
analuisavier <analuivieira@hotmail.com>
2025-06-14 00:05:35 -0300

    Alterações

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from dados_iniciais import pessoas, empresas, categorias, percentuais
+
 # Códigos de escape ANSI
 cor_vermelho = "\033[31m"
 cor_verde = "\033[32m" 
@@ -8,3 +10,14 @@ cor_ciano = "\033[36m"
 cor_branco = "\033[37m"
 italico = "\033[3m"
 reset = "\033[0m"
+
+def main():
+    print("[SIMULADOR DE MERCADO]")
+    print(f"{len(pessoas)} pessoas cadastradas.")
+    print(f"{len(empresas)} empresas cadastradas.")
+    print("Categorias disponíveis:")
+    for categoria in categorias:
+        print(f"- {categoria}")
+
+if __name__ == "__main__":
+    main()
